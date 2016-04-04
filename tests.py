675456781a04849d989f648878a8a836a7df3c74
konstin <konstin@konstin.no-ip.biz>
2016-04-04 15:28:33 +0200

    Unit tests for replaces

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ Contains unit tests for yii-to-yii2.py
 """
 
 import unittest
-from yii-to-yii2 import *
+from yii_to_yii2 import *
 
 class TestStringMethods(unittest.TestCase):
     def test_activequery(self):
@@ -15,6 +15,31 @@ class TestStringMethods(unittest.TestCase):
         ]
         for i in contents:
             result = activequery(i[0])
+            self.assertEqual(result, i[1])
+           
+    def test_replaces(self):
+        contents = [
+            ("Yii::app()", "Yii::$app"),
+            ("html/", "web/"),
+            ("CActiveRecord", "ActiveRecord"),
+            ("Yii::$app->createUrl('controller/page')", "Url::to('controller/page')"),
+            ("$this->createUrl('controller/page')", "Url::to('controller/page')"),
+            ("Html::link('controller/page')", "Html::a('controller/page')")
+        ]
+        replacements = [
+            "yii-app",
+            "html-web",
+            "yii1-classes",
+            "create-url",
+            "static-table-name",
+            "this-context",
+            "render",
+            "layout"
+        ]
+        with open("yii1-classes.txt", 'r') as file:
+            yii1_classes = [i.strip() for i in file.readlines()]
+        for i in contents:
+            result = replace(i[0], yii1_classes, replacements)
             self.assertEqual(result, i[1])
 
 
