e7e625ec94122cd8eb8d7d22cab12693b1b4e4c7
konstin <konstin@mailbox.org>
2016-11-14 12:40:05 +0100

    External object lists and object parsing and spitting

---
+++ src/lib.rs
@@ -0,0 +1,228 @@
+#[macro_use] extern crate json;
+extern crate hyper;
+extern crate chrono;
+
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+use std::fs::File;
+
+use chrono::*;
+use json::JsonValue;
+use hyper::Url;
+
+#[cfg(test)]
+mod test;
+
+pub struct ExternalList {
+    url: String,
+    response: Option<JsonValue>,
+}
+
+impl ExternalList {
+    pub fn new(url: String) -> ExternalList {
+        ExternalList { url: url, response: None }
+    }
+}
+
+impl ExternalList {
+    fn download_url(&mut self) -> Option<JsonValue> {
+        let client = hyper::Client::new();
+        let mut res = client.get(&self.url).send().unwrap();
+        assert_eq!(res.status, hyper::Ok);
+        let mut json_string = String::new();
+        res.read_to_string(&mut json_string).unwrap();
+        Some(json::parse(&json_string).unwrap())
+    }
+}
+
+impl Iterator for ExternalList {
+    type Item = json::JsonValue;
+
+    fn next(&mut self) -> Option<json::JsonValue> {
+        if self.response.is_none() {
+            self.response = self.download_url()
+        }
+
+        let mut load_required = false;
+        if let Some(ref mut response_ref) = self.response {
+            if response_ref["data"].len() == 0 {
+                if response_ref["links"].entries().any(|(x, _)| x == "next") {
+                    self.url = response_ref["links"]["next"].to_string();
+                    load_required = true;
+                } else {
+                    return None;
+                }
+            }
+        }
+
+        if load_required {
+            self.response = self.download_url();
+        }
+
+        // This unwrap can't fail as download_url will set response to some
+        if let Some(ref mut response_ref) = self.response {
+            if let JsonValue::Array(ref mut data) = response_ref["data"] {
+                return Some(data.remove(0));
+            } else {
+                println!("Broken 1");
+                return None;
+            }
+        } else {
+            println!("Broken 2");
+            return None;
+        }
+    }
+}
+
+fn add_external_list(url: &str) {
+    println!("Adding External List: {}", url)
+}
+
+pub fn url_to_path(url: &str) -> PathBuf {
+    let cachefolder = "/home/konsti/cache-rust/";
+    let mut url: Url = Url::from_str(url).unwrap();
+
+    // Remove the oparl filters
+    // Those parameters shouldn't be parsed on anyway, but just in case we'll do this
+    let url_binding: Url = url.clone();
+    let query_without_filters = url_binding.query_pairs()
+        .filter(|&(ref x, _)| x != "modified_until")
+        .filter(|&(ref x, _)| x != "modified_since")
+        .filter(|&(ref x, _)| x != "created_since")
+        .filter(|&(ref x, _)| x != "created_until");
+
+    let url = url.query_pairs_mut()
+        .clear()
+        .extend_pairs(query_without_filters)
+        .finish();
+
+    // Prepare parts
+    let port: String = url.port().map_or("".to_string(), |port| ":".to_string() + &port.to_string());
+    let mut path = url.path().to_string();
+    if path.ends_with("/") {
+        path.pop();
+    };
+
+    // Get path
+    let mut cachefile = cachefolder.to_string() + url.scheme() + ":" + url.host_str().unwrap_or("");
+    cachefile += &port;
+    cachefile += &path;
+    if let Some(q) = url.query() {
+        if q != "" {
+            cachefile += "?";
+            cachefile += q;
+        }
+    }
+    cachefile += ".json";
+
+    Path::new(&cachefile).to_path_buf()
+}
+
+fn write_to_cache(url: &str, object: &JsonValue) {
+    let filepath = url_to_path(url);
+    println!("Writen to Cache:");
+    println!("  Path: {}", filepath.display());
+    println!("  Object-id: {}", object["id"]);
+
+    let file = File::open(filepath);
+}
+
+fn parse_entry(key: String, entry: &mut JsonValue, entry_def: &JsonValue) {
+    let external_lists: Vec<JsonValue> = Vec::new();
+
+    if entry_def["type"] == "array" {
+        let mut i = 0;
+        while i < entry.members().len() {
+            parse_entry(String::from(key.as_str()) + "[" + &i.to_string() + "]", &mut entry[i], &entry_def["items"]);
+            i += 1;
+        }
+    } else if entry_def["type"] == "object" {
+        if entry["type"] == "Feature" {
+            return; // GeoJson is treated is a single value
+        }
+        parse_object(entry);
+        *entry = JsonValue::String(entry["id"].to_string());
+    } else if entry_def.members().any(|i| i == "entrydef") && entry_def["references"] == "externalList" {
+        if !external_lists.iter().map(|i| i["url"].to_string()).any(|x| entry == &x) {
+            add_external_list(&format!("{{\"url\": \"{}\", \"last_update\": null", entry));
+        }
+    }
+}
+
+
+fn parse_object(target: &mut JsonValue) {
+    // Load the schema
+    let mut schema = JsonValue::new_array();
+    for i in Path::new("/home/konsti/oparl/schema/").read_dir().unwrap() {
+        let mut f: File = File::open(i.unwrap().path()).unwrap();
+        let mut s = String::new();
+        f.read_to_string(&mut s).unwrap();
+        let x = json::parse(&s).unwrap();
+        let y = x["title"].to_string();
+        schema[y] = x;
+    }
+
+    assert_eq!(schema.len(), 12);
+
+    let let_binding = target["type"].to_string();
+    let oparl_type = let_binding.split("/").last().unwrap();
+    let spec_for_object = &schema[oparl_type]["properties"];
+
+    for (key, mut value) in target.entries_mut() {
+        // Check if the key is defined in the specification
+        if spec_for_object.entries().map(|(key, _)| key).any(|i| i == key) {
+            parse_entry(key.to_string(), &mut value, &spec_for_object[key]);
+        }
+    }
+
+    write_to_cache(target["id"].as_str().unwrap(), &target)
+}
+
+pub fn parse_external_list(url_raw: String, last_sync: Option<String>) {
+    let this_sync = Local::now().format("%Y-%m-%dT%H:%M:%S%Z").to_string();
+
+    let limit: Option<usize> = None;
+    let mut url: Url = Url::parse(&url_raw).unwrap();
+
+    if let Some(last_sync_time) = last_sync {
+        // Add the modified_since filter
+        url.query_pairs_mut().append_pair("modified_since", &last_sync_time).finish();
+    }
+
+    let mut elist = ExternalList::new(url.to_string());
+
+    /*if let Some(x) = limit {
+        elist = elist.take(x);
+    }*/
+
+    let mut urls = Vec::new();
+    for mut i in elist {
+        parse_object(&mut i);
+        urls.push(i["id"].to_string());
+    }
+
+    let mut old_urls = Vec::new();
+    urls.append(&mut old_urls);
+
+
+    let mut urls_as_json = JsonValue::new_array();
+    for i in urls {
+        urls_as_json.push(i).unwrap();
+    }
+    write_to_cache(&url_raw, &urls_as_json);
+
+    // self.external_lists_lock.acquire()
+    let mut external_lists = JsonValue::from("[]");
+    for i in external_lists.members_mut() {
+        if i["url"] == url_raw {
+            i["last_update"] = JsonValue::String(this_sync);
+            break;
+        }
+    };
+
+    // save();
+    // self.external_lists_lock.release()
+}
+
+
---
+++ src/test.rs
@@ -0,0 +1,79 @@
+use chrono::*;
+use json;
+use hyper::Url;
+
+use std::path::Path;
+use std::str::FromStr;
+
+#[test]
+fn test_parse_object() {
+    let mut input = object! {
+        "id" => "http://localhost:8080/oparl/v1.0/paper/2",
+        "type" => "https://schema.oparl.org/1.0/Paper",
+        "auxiliaryFile" => array![
+            object!{
+              "id" => "http://localhost:8080/oparl/v1.0/file/2",
+              "type" => "https://schema.oparl.org/1.0/File",
+              "accessUrl" => "http://localhost:8080/fileaccess/access/2",
+              "created" => "2016-05-02T19:53:08+02:00",
+              "modified" => "2016-05-02T19:53:08+02:00"
+            }
+        ],
+        "created" => "2016-05-02T00:00:00+02:00",
+        "modified" => "2016-05-02T00:00:00+02:00"
+    };
+
+    let expected_output = object! {
+        "id" => "http://localhost:8080/oparl/v1.0/paper/2",
+        "type" => "https://schema.oparl.org/1.0/Paper",
+        "auxiliaryFile" => array! [
+            "http://localhost:8080/oparl/v1.0/file/2"
+        ],
+        "created" => "2016-05-02T00:00:00+02:00",
+        "modified" => "2016-05-02T00:00:00+02:00"
+    };
+
+    super::parse_object(&mut input);
+    assert_eq!(input, expected_output);
+}
+
+#[test]
+fn test_parse_external_list() {
+    let eurl = "http://localhost:8080/oparl/v1.0/body/0/list/paper?a=b";
+    let time = Local::now().format("%Y-%m-%dT%H:%M:%S%Z").to_string();
+    super::parse_external_list(eurl.to_string(), Some(time));
+}
+
+#[test]
+fn test_external_list_iterator() {
+    let expected_ids = [
+        "http://localhost:8080/oparl/v1.0/paper/1",
+        "http://localhost:8080/oparl/v1.0/paper/2",
+        "http://localhost:8080/oparl/v1.0/paper/3",
+        "http://localhost:8080/oparl/v1.0/paper/4",
+        "http://localhost:8080/oparl/v1.0/paper/5",
+        "http://localhost:8080/oparl/v1.0/paper/6",
+        "http://localhost:8080/oparl/v1.0/paper/7",
+    ];
+
+    let eurl = "http://localhost:8080/oparl/v1.0/body/0/list/paper";
+    let list = super::ExternalList::new(eurl.to_string());
+    let ids = list.map(|i| i["id"].to_owned()).collect::<Vec<_>>();
+    assert_eq!(ids, expected_ids);
+}
+
+#[test]
+fn test_url_to_path() {
+    let data = vec![
+        ("https://example.tld:8080/oparl/v1.0/paper/1", "/home/konsti/cache-rust/https:example.tld:8080/oparl/v1.0/paper/1.json"),
+        ("https://example.tld/oparl/v1.0/paper/1", "/home/konsti/cache-rust/https:example.tld/oparl/v1.0/paper/1.json"),
+        ("https://example.tld", "/home/konsti/cache-rust/https:example.tld.json"),
+    ];
+
+    for (url, path) in data {
+        assert_eq! (super::url_to_path(url), Path::new(path));
+        assert_eq! (super::url_to_path(&(url.to_string() + "/")), Path::new(path));
+    }
+
+    // let x = "http://localhost:8080/oparl/v1.0/body/0/list/paper?id=3&modified_until=2016-05-03T00%3A00%3A00%2B02%3A00&asd=asx";
+}
